9cd16229738f8039e87d3cc840daae354b8bf131
alexander.shustanov <alexander@openvessel.io>
2024-03-07 12:48:18 +0400

    changes

--- src/stories/pages/create/Create.stories.tsx
+++ src/stories/pages/create/Create.stories.tsx
@@ -1,95 +1,137 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import {
-  AdminContext,
-  Create,
-  defaultI18nProvider,
-  TextInput,
-  DateInput,
-  SimpleForm,
-  TopToolbar,
-  Button,
-  ResourceContextProvider,
+    AdminContext,
+    Button,
+    Create,
+    DateInput,
+    defaultI18nProvider,
+    required,
+    ResourceContextProvider,
+    SimpleForm,
+    TextInput,
+    TopToolbar,
+    useSaveContext,
 } from "react-admin";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import { dataProvider } from "../../../dataProvider";
-import React from "react";
+import React, { useState } from "react";
+import { Dialog, DialogTitle } from "@mui/material";
+import { FieldValues } from "react-hook-form";
 
 const meta = {
-  title: "Pages/Create",
-  component: Create as any,
-  decorators: [(Story) => defaultDecorator(Story)],
+    title: "Pages/Create",
+    component: Create as any,
+    decorators: [(Story) => defaultDecorator(Story)],
 } satisfies Meta<typeof Create>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: (props) => {
-    return (
-      <Create {...props}>
-        <SimpleForm>
-          <TextInput source="id" />
-          <TextInput source="name" />
-          <DateInput source="birthday" />
-        </SimpleForm>
-      </Create>
-    );
-  },
+    render: (props) => {
+        return (
+            <Create {...props}>
+                <SimpleForm>
+                    <TextInput source="id"/>
+                    <TextInput source="name"/>
+                    <DateInput source="birthday"/>
+                </SimpleForm>
+            </Create>
+        );
+    },
 };
 
 export const CustomActions: Story = {
-  render: (props) => {
-    const handleClick = () => {
-      alert('Custom action result');
-    }
+    render: (props) => {
+        const handleClick = () => {
+            alert('Custom action result');
+        }
 
-    const CustomActions = () => (
-      <TopToolbar>
-        <Button color="primary" onClick={handleClick} label="Custom Action" />
-      </TopToolbar>
-    );
+        const CustomActions = () => (
+            <TopToolbar>
+                <Button color="primary" onClick={handleClick} label="Custom Action"/>
+            </TopToolbar>
+        );
 
-    return (
-      <Create actions={<CustomActions />} {...props}>
-        <SimpleForm>
-          <TextInput source="id" />
-          <TextInput source="name" />
-          <DateInput source="birthday" />
-        </SimpleForm>
-      </Create>
-    );
-  },
+        return (
+            <Create actions={<CustomActions/>} {...props}>
+                <SimpleForm>
+                    <TextInput source="id"/>
+                    <TextInput source="name"/>
+                    <DateInput source="birthday"/>
+                </SimpleForm>
+            </Create>
+        );
+    },
 };
 
 export const Aside: Story = {
-  render: (props) => {
-    const Aside = () => (
-      <Box sx={{ paddingX: 4, paddingY: 8, width: 300 }}>
-        <Stack spacing={2}>
-          <Typography variant="h6">Aside content</Typography>
-          <Typography>Additional aside content</Typography>
-        </Stack>
-      </Box>
-    );
+    render: (props) => {
+        const Aside = () => (
+            <Box sx={{ paddingX: 4, paddingY: 8, width: 300 }}>
+                <Stack spacing={2}>
+                    <Typography variant="h6">Aside content</Typography>
+                    <Typography>Additional aside content</Typography>
+                </Stack>
+            </Box>
+        );
 
-    return (
-      <Create aside={<Aside />} {...props}>
-        <SimpleForm>
-          <TextInput source="id" />
-          <TextInput source="name" />
-          <DateInput source="birthday" />
-        </SimpleForm>
-      </Create>
-    );
-  },
+        return (
+            <Create aside={<Aside/>} {...props}>
+                <SimpleForm>
+                    <TextInput source="id"/>
+                    <TextInput source="name"/>
+                    <DateInput source="birthday"/>
+                </SimpleForm>
+            </Create>
+        );
+    },
+};
+
+export const CreateDialog: Story = {
+    render: () => {
+        const [open, setOpen] = useState(false);
+        const MyForm = () => {
+            let {
+                save,
+            } = useSaveContext();
+
+            const onSubmit = async (values: FieldValues) => {
+                if (save) {
+                    await save(values, {
+                        onSuccess: () => setOpen(false),
+                    });
+                }
+            };
+
+            return <SimpleForm onSubmit={onSubmit}>
+                <TextInput source="id"/>
+                <TextInput source="name" validate={required()}/>
+                <DateInput source="birthday"/>
+            </SimpleForm>;
+        };
+
+        return (
+            <>
+                <Button onClick={() => setOpen(true)} label="Open Dialog"/>
+
+                <Dialog open={open}>
+                    <DialogTitle>Create User</DialogTitle>
+                    <Create>
+                        <MyForm/>
+                    </Create>
+                </Dialog>
+            </>
+        );
+    },
 };
 
 const defaultDecorator = (Story: () => React.JSX.Element) => (
-  <AdminContext dataProvider={dataProvider} i18nProvider={defaultI18nProvider}>
-    <ResourceContextProvider value="users">
-      <Story />
-    </ResourceContextProvider>
-  </AdminContext>
+    <AdminContext dataProvider={dataProvider} i18nProvider={defaultI18nProvider}>
+        <ResourceContextProvider value="users">
+            <Story/>
+        </ResourceContextProvider>
+    </AdminContext>
 );
--- src/stories/pages/edit/Edit.stories.tsx
+++ src/stories/pages/edit/Edit.stories.tsx
@@ -1,14 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import {
-  AdminContext,
-  Button,
-  DateInput,
-  Edit,
-  ResourceContextProvider,
-  SimpleForm,
-  TextInput,
-  TopToolbar,
-  defaultI18nProvider,
+    AdminContext,
+    Button,
+    CreateButton,
+    Datagrid,
+    DateInput,
+    defaultI18nProvider,
+    Edit,
+    List,
+    ResourceContextProvider,
+    SimpleForm,
+    TextField,
+    TextInput,
+    TopToolbar,
+    useEditContext,
 } from "react-admin";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -17,81 +22,110 @@ import { dataProvider } from "../../../dataProvider";
 import React from "react";
 
 const meta = {
-  title: "Pages/Edit",
-  component: Edit as any,
-  decorators: [(Story) => defaultDecorator(Story)],
-  args: {
-    id: 2
-  },
+    title: "Pages/Edit",
+    component: Edit as any,
+    decorators: [(Story) => defaultDecorator(Story)],
+    args: {
+        id: 2
+    },
 } satisfies Meta<typeof Edit>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta> ;
 export const Default: Story = {
-  render: (props) => {
-    return (
-      <Edit resource={'users'} {...props}>
-        <SimpleForm>
-          <TextInput source="id" />
-          <TextInput source="name" />
-          <DateInput source="birthday" />
-        </SimpleForm>
-      </Edit>
-    );
-  },
+    render: (props) => {
+        return (
+            <Edit resource={'users'} {...props}>
+                <SimpleForm>
+                    <TextInput source="id"/>
+                    <TextInput source="name"/>
+                    <DateInput source="birthday"/>
+                </SimpleForm>
+            </Edit>
+        );
+    },
 };
 
 export const CustomActions: Story = {
-  render: (props) => {
-    const handleClick = () => {
-      alert("Custom action result");
-    };
+    render: (props) => {
+        const handleClick = () => {
+            alert("Custom action result");
+        };
 
-    const CustomActions = () => (
-      <TopToolbar>
-        <Button color="primary" onClick={handleClick} label="Custom Action" />
-      </TopToolbar>
-    );
+        const CustomActions = () => (
+            <TopToolbar>
+                <Button color="primary" onClick={handleClick} label="Custom Action"/>
+            </TopToolbar>
+        );
 
-    return (
-      <Edit actions={<CustomActions />} {...props}>
-        <SimpleForm>
-          <TextInput source="id" />
-          <TextInput source="name" />
-          <DateInput source="birthday" />
-        </SimpleForm>
-      </Edit>
-    );
-  },
+        return (
+            <Edit actions={<CustomActions/>} {...props}>
+                <SimpleForm>
+                    <TextInput source="id"/>
+                    <TextInput source="name"/>
+                    <DateInput source="birthday"/>
+                </SimpleForm>
+            </Edit>
+        );
+    },
 };
 
 export const Aside: Story = {
-  render: (props) => {
-    const Aside = () => (
-      <Box sx={{ paddingX: 4, paddingY: 8, width: 300 }}>
-        <Stack spacing={2}>
-          <Typography variant="h6">Aside content</Typography>
-          <Typography>Additional aside content</Typography>
-        </Stack>
-      </Box>
-    );
+    render: (props) => {
+        const Aside = () => (
+            <Box sx={{ paddingX: 4, paddingY: 8, width: 300 }}>
+                <Stack spacing={2}>
+                    <Typography variant="h6">Aside content</Typography>
+                    <Typography>Additional aside content</Typography>
+                </Stack>
+            </Box>
+        );
 
-    return (
-      <Edit aside={<Aside />} {...props}>
-        <SimpleForm>
-          <TextInput source="id" />
-          <TextInput source="name" />
-          <DateInput source="birthday" />
-        </SimpleForm>
-      </Edit>
-    );
-  },
+        return (
+            <Edit aside={<Aside/>} {...props}>
+                <SimpleForm>
+                    <TextInput source="id"/>
+                    <TextInput source="name"/>
+                    <DateInput source="birthday"/>
+                </SimpleForm>
+            </Edit>
+        );
+    },
+};
+
+export const SubResource: Story = {
+    render: (props) => {
+
+        const TasksTable = () => {
+            const { record: user } = useEditContext();
+
+            if (!user) return <></>;
+
+            return <List resource="tasks"
+                         filter={{ user_id: user.id }}
+                         actions={<><CreateButton state={{ record: { user_id: user.id } }}/></>}>
+                <Datagrid>
+                    <TextField source="name"/>
+                </Datagrid>
+            </List>;
+        };
+
+        return <Edit resource='users' {...props}>
+            <SimpleForm>
+                <TextInput source="id"/>
+                <TextInput source="name"/>
+                <DateInput source="birthday"/>
+
+                <TasksTable/>
+            </SimpleForm>
+        </Edit>;
+    }
 };
 
 const defaultDecorator = (Story: () => React.JSX.Element) => (
-  <AdminContext dataProvider={dataProvider} i18nProvider={defaultI18nProvider}>
-    <ResourceContextProvider value="users">
-      <Story />
-    </ResourceContextProvider>
-  </AdminContext>
+    <AdminContext dataProvider={dataProvider} i18nProvider={defaultI18nProvider}>
+        <ResourceContextProvider value="users">
+            <Story/>
+        </ResourceContextProvider>
+    </AdminContext>
 );
